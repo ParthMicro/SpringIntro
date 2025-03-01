0000fc14ec138bdaa3b403138fed41e15686e639
ParthMicro <goyalparth058@gmail.com>
2025-03-01 15:29:05 +0530

    hello message with params

--- src/main/java/com/example/hellomessagingapp/controller/HelloRestController.java
+++ src/main/java/com/example/hellomessagingapp/controller/HelloRestController.java
@@ -1,12 +1,19 @@
 package com.example.hellomessagingapp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloRestController {
     @RequestMapping("/")
     public String sayhello(){
         return "Hello From BridgeLabz";
+    }
+
+    @GetMapping("/hello/query")
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 
 }
